3e0a64eed1b689c958193c75799569608d0cdfb7
Kris Cottingham <kris.cottingham@redfin.com>
2025-09-21 21:54:26 -0500

    fix: use returned value on halt

--- reduce_while.go
+++ reduce_while.go
@@ -3,13 +3,10 @@ package gocollections
 func ReduceWhile[E ~[]I, I, A any](enumerable E, initial A, fun func(I, A) (A, bool)) A {
 	accumulator := initial
 	for _, item := range enumerable {
-		var new_accumulator A
 		var cont bool
 
-		new_accumulator, cont = fun(item, accumulator)
-		if cont {
-			accumulator = new_accumulator
-		} else {
+		accumulator, cont = fun(item, accumulator)
+		if !cont {
 			break
 		}
 	}
